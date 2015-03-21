95e69b92bb782082627a8219cb042b23d5dbdf5e
Nicolas Alejandro Nunez <nanunezr@uark.edu>
2015-03-21 14:45:44 -0500

    datahandler class update

--- desktop/src/desktop/DataHandler.java
+++ desktop/src/desktop/DataHandler.java
@@ -9,6 +9,8 @@ import oracle.jdbc.pool.OracleDataSource;
 /**
  *
  * @author  Nicolas Nunez
+ * Some notes: in order to add date this is the format 'dd/mmm/yyyy'
+ * where mmm is MAR for example
  */
 
 public class DataHandler {
@@ -36,7 +38,7 @@ public class DataHandler {
     public void addNewEmployee(int id, String lastname, String firstname, String dob,String position, 
                                int salary, String phoneno) throws SQLException {
         Statement stmt = conn.createStatement();
-        query = "INSERT INTO Employee VALUES("+id+", "+lastname+", "+firstname+", "+dob+", "+position+", "+salary+", "+phoneno+")";
+        query = "INSERT INTO Employee VALUES("+id+", '"+lastname+"', '"+firstname+"', '"+dob+"', '"+position+"', "+salary+", '"+phoneno+"')";
         System.out.println("\nExecuting query: " + query);
         stmt.execute(query);
     }
@@ -69,9 +71,23 @@ public class DataHandler {
                 temp.getInt(6) + " " + temp.getString(7));
         }
     }
+    public void printInventory() throws SQLException{
+        ResultSet temp = getAllProducts();
+        while(temp.next()){
+            System.out.println(temp.getInt(1)+ " " + temp.getString(2) + " " +
+                temp.getInt(3) + " " + temp.getString(4).substring(0, 9)+ " " + temp.getFloat(5) + " " + 
+                temp.getFloat(6) + " " + temp.getFloat(7) + " " + temp.getInt(8) + " " + 
+                temp.getString(9) + " " + temp.getString(10));
+        }
+    }
+
     
     public static void main(String[] args) throws SQLException {
         DataHandler datahandler = new DataHandler();
         datahandler.printEmployees();
+        datahandler.addNewEmployee(6, "McAlly", "James", "05/MAR/1991","Cashier", 
+                               30000, "479-990-7869");
+        datahandler.printEmployees();
+        //datahandler.printInventory();
     }
 }
