67b4b52f919b66fe876459c99f6adfb00484224b
Brandon Foster <bxf008@email.uark.edu>
2015-03-21 18:58:26 -0500

    Keypad

--- desktop/src/desktop/Table.java
+++ desktop/src/desktop/Table.java
@@ -1,14 +1,37 @@
 package desktop;
 
+import java.util.ArrayList;
+
 public class Table {
     private int tableNumber;
+    private int tableEmployee;
+    private ArrayList<Integer> tableItemsOrdered = new ArrayList();
     
     public Table() {
         super();
-        tableNumber = 0;
+        tableNumber = -1;
+        tableEmployee = -1;
+    }
+    
+    public Table(int tNum, int tEmp){
+        tableNumber = tNum;
+        tableEmployee = tEmp;
     }
     
     public void setTableNumber(int n){
         this.tableNumber = n;
     }
+    
+    public void setTableEmployee(int e){
+        this.tableEmployee = e;
+    }
+    
+    public void addTableItem(int item){
+        tableItemsOrdered.add(item);
+    }
+    
+    public void removeTableItem(int item){
+        tableItemsOrdered.remove(item);
+    }
+
 }
